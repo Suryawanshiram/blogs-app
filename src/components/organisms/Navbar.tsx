121c135e23e6e0a51356b96248691b359ca8f9d3
Suryawanshiram <suryawanshiram2@gmail.com>
2025-03-09 12:17:01 +0530

    refactor: getting session from client refactored

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,23 +1,25 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
 import {
   RegisterLink,
   LoginLink,
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { buttonVariants } from "../ui/button";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
-const Navbar = async () => {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+const Navbar = () => {
+  const { getUser } = useKindeBrowserClient();
+  const user = getUser();
+  console.log(user);
 
   return (
     <nav className="flex items-center justify-between w-full h-16 px-4 py-2 bg-white shadow-md">
       <div className="px-5 flex items-center gap-4 justify-between w-full">
         <Link href="/">
           <h1 className="text-amber-600 text-3xl font-semibold">
-            Blogs<span className="text-blue-600">Ram</span>
+            Tech<span className="text-blue-600">Blogs</span>
           </h1>
         </Link>
 
@@ -37,8 +39,8 @@ const Navbar = async () => {
 
       <div className="flex gap-2 w-full justify-end">
         {user ? (
-          <div className="flex gap-2">
-            <p className="py-2 rounded-md border px-2 text-green-500">
+          <div className="flex gap-4 w-full justify-end">
+            <p className="rounded-md border px-3 py-1.5 text-green-500">
               {user.given_name}
             </p>
             <LogoutLink className={buttonVariants({ variant: "secondary" })}>
