77604909211396b81291fd132fcf2ef19435863c
Suryawanshiram <suryawanshiram2@gmail.com>
2025-03-09 18:17:56 +0530

    feat: added img url config

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,11 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         port:"",
       },
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn0.gstatic.com",
+        port:"",
+      },
     ],
   }
 };
